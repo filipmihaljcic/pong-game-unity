c81c2eb8cb2a2b6922387e5b117c272bd486aebb
Filip Mihaljčić <78499417+filipmihaljcic@users.noreply.github.com>
2022-05-06 14:21:05 +0200

    Update ComputerPaddle.cs

--- Pong Game/Assets/Scripts/ComputerPaddle.cs
+++ Pong Game/Assets/Scripts/ComputerPaddle.cs
@@ -8,21 +8,23 @@ namespace Project.Scripts
 
         private void FixedUpdate() 
         {
-            if (_ball.velocity.x > 0.0f) // the ball is moving towards the computer paddle 
+            // the ball is moving towards the computer paddle 
+            if (_ball.velocity.x > 0.0f) 
             {
                 // if the ball position is higher than computer paddle position
                 if (_ball.transform.position.y > transform.position.y) 
-                    // move paddle down
+                    // move computer paddle up
                     _rb2D.AddForce(Vector2.up * _speed);
 
                 else if (_ball.transform.position.y < transform.position.y)
                     _rb2D.AddForce(Vector2.down * _speed);
             }
+            // this ensures that computer paddle will always be in the middle 
             else 
             {
                 // if computer paddle is above middle position 
                 if (transform.position.y > 0.0f)
-                    // move down
+                    // move computer paddle down
                     _rb2D.AddForce(Vector2.down * _speed);
 
                 else if (transform.position.y < 0.0f)
